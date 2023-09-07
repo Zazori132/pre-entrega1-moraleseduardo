6bd3e380c9db4139500cd45070825db133a68b50
Eduardo <edi.uch17@gmail.com>
2023-09-06 21:11:29 -0600

    Cambio y actualizacion 06/09/2023

--- juego.js
+++ juego.js
@@ -1,38 +1,49 @@
 // Generar un número aleatorio entre 1 y 50
 function generarNumeroSecreto() {
-    return Math.floor(Math.random() * 50) + 1;
+  return Math.floor(Math.random() * 50) + 1;
+}
+
+// Verificar si la suposición es correcta y proporcionar pistas
+function verificarSuposicion(suposicionUsuario, numeroSecreto) {
+  if (suposicionUsuario === numeroSecreto) {
+    return "¡Felicitaciones! Adivinaste el número en " + (intentosRealizados + 1) + " intentos.";
+  } else if (suposicionUsuario < numeroSecreto) {
+    return "El número es mayor.";
+  } else {
+    return "El número es menor.";
   }
-  
-  // Verificar si la suposición es correcta y proporcionar pistas
-  function verificarSuposicion(suposicionUsuario, numeroSecreto) {
-    if (suposicionUsuario === numeroSecreto) {
-      console.log("¡Felicitaciones! Adivinaste el número en " + (intentosRealizados + 1) + " intentos.");
-      return true;
-    } else if (suposicionUsuario < numeroSecreto) {
-      console.log("El número es mayor.");
-    } else {
-      console.log("El número es menor.");
-    }
-    return false;
+}
+
+console.log("¡Bienvenido al juego de adivinar el número!");
+
+const numeroSecreto = generarNumeroSecreto();
+const intentosMaximos = 10;
+let intentosRealizados = 0;
+const suposicionesAnteriores = [];
+
+while (intentosRealizados < intentosMaximos) {
+  const suposicionUsuario = parseInt(prompt("Intento " + (intentosRealizados + 1) + ": Adivina el número (entre 1 y 50):"));
+
+  // Manejo de errores si el usuario ingresa algo que no es un número
+  if (isNaN(suposicionUsuario)) {
+    console.log("Por favor, ingresa un número válido.");
+    continue;
   }
-  
-  console.log("¡Bienvenido al juego de adivinar el número!");
-  
-  const numeroSecreto = generarNumeroSecreto();
-  const intentosMaximos = 5;
-  let intentosRealizados = 0;
-  
-  while (intentosRealizados < intentosMaximos) {
-    const suposicionUsuario = parseInt(prompt("Intento " + (intentosRealizados + 1) + ": Adivina el número (entre 1 y 50):"));
-    
-    if (verificarSuposicion(suposicionUsuario, numeroSecreto)) {
-      break;
-    }
-    
-    intentosRealizados++;
+
+  suposicionesAnteriores.push(suposicionUsuario);
+
+  const resultado = verificarSuposicion(suposicionUsuario, numeroSecreto);
+  console.log(resultado);
+
+  if (suposicionUsuario === numeroSecreto) {
+    break;
   }
-  
-  if (intentosRealizados === intentosMaximos) {
-    console.log("Lo siento, has agotado tus " + intentosMaximos + " intentos. El número correcto era " + numeroSecreto + ".");
-  }
-  
+
+  intentosRealizados++;
+}
+
+if (intentosRealizados === intentosMaximos) {
+  console.log("Lo siento, has agotado tus " + intentosMaximos + " intentos. El número correcto era " + numeroSecreto + ".");
+}
+
+console.log("Tus suposiciones anteriores fueron: " + suposicionesAnteriores.join(', '));
